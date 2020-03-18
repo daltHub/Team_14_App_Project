9056571343eb6427440171d12f315bc30e111f46
claoidekemp <60619981+claoidekemp@users.noreply.github.com>
2020-03-18 20:59:48 +0000

    Made it pretty with new colour scheme and buttons

--- ProjectManager/app/src/main/java/com/example/projectmanager/Voting.java
+++ ProjectManager/app/src/main/java/com/example/projectmanager/Voting.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -167,6 +168,23 @@ public class Voting extends AppCompatActivity {
         });
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_homescreen:
+                Intent goHome = new Intent(this, Homescreen.class);
+                startActivity(goHome);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 //    private void writeobject(String name, Users user){
