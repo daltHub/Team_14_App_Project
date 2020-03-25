c8c53369254c07233cc103026f3b813fad9c577a
daltHub <57568091+daltHub@users.noreply.github.com>
2020-03-25 18:06:46 +0000

    still broken

--- ProjectManager/app/src/main/java/com/example/projectmanager/Message.java
+++ ProjectManager/app/src/main/java/com/example/projectmanager/Message.java
@@ -1,25 +1,60 @@
 package com.example.projectmanager;
 
 public class Message {
-    private String text;
-    private MemberData memberData;
-    private boolean belongsToCurrentUser;
 
-    public Message(String text, MemberData data, boolean belongsToCurrentUser) {
+    private String id;
+    private String text;
+    private String name;
+    private String photoUrl;
+    private String imageUrl;
+
+    public Message() {
+    }
+
+    public Message(String text, String name, String photoUrl, String imageUrl) {
         this.text = text;
-        this.memberData = data;
-        this.belongsToCurrentUser = belongsToCurrentUser;
+        this.name = name;
+        this.photoUrl = photoUrl;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
     }
 
     public String getText() {
         return text;
     }
 
-    public MemberData getMemberData() {
-        return memberData;
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
     }
 
-    public boolean isBelongsToCurrentUser() {
-        return belongsToCurrentUser;
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
